8b3a30439b5c4cdbe9fbf13e88f56ead6614e359
Kevin <vnctkevin@gmail.com>
2023-08-02 15:52:16 +0700

    update profile page picture

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -42,7 +42,6 @@ export default function AboutPage(props: AboutPageProps) {
                             />
                 </div>
                 <div></div>
-            
             </div>
             
 
