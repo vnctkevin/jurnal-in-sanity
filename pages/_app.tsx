4097132597191b47cce79e694aec984310a9d9ec
Kevin <vnctkevin@gmail.com>
2023-12-26 14:42:00 +0700

    add SpeedInsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app'
 import { lazy } from 'react'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export interface SharedPageProps {
   draftMode: boolean
@@ -26,6 +27,7 @@ export default function App({
         <>
           <Component {...pageProps} />
           <Analytics />
+          <SpeedInsights />
         </>
       )}
     </>
