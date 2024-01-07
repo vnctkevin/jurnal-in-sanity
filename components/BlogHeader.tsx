4aa29f37b65417b0df284a6ebe5940309f39a6e0
Kevin <vnctkevin@gmail.com>
2024-01-08 01:21:45 +0700

    ui update on jvk

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -15,6 +15,7 @@ export default function BlogHeader({
   switch (level) {
     case 1:
       return (
+        <>
         <header className="mb-10 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
           <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
             {title}
@@ -25,6 +26,8 @@ export default function BlogHeader({
             <PortableText value={description} />
           </h4>
         </header>
+        <hr className="border-accent-3 mb-12" />
+        </>
       )
 
     case 2:
--- components/MoreStories.tsx
+++ components/MoreStories.tsx
@@ -4,7 +4,7 @@ import type { Post } from 'lib/sanity.queries'
 export default function MoreStories({ posts }: { posts: Post[] }) {
   return (
     <section>
-      <div className="mb-32 grid grid-cols-1 gap-y-20 md:grid-cols-2 md:gap-x-16 md:gap-y-32 lg:gap-x-32">
+      <div className="mb-32 grid grid-cols-1 gap-y-20 md:grid-cols-2 lg:grid-cols-3 md:gap-x-16 md:gap-y-24 lg:gap-x-28">
         {posts.map((post) => (
           <PostPreview
             key={post._id}
