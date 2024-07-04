c873e8ba56fe0f98a12226978a60aa552e7d1b79
Kevin <vnctkevin@gmail.com>
2024-07-04 18:52:17 +0700

    troubleshooting firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { lazy } from 'react'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AppProps } from 'next/app'
+import { firebase } from '@/lib/firebase/firebaseClient'
 import Script from "next/script";
 
 import 'tailwindcss/tailwind.css'
@@ -20,13 +21,13 @@ export default function App({
   const { draftMode, token } = pageProps
   return (
     <>
-      <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+      <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.FIREBASE_MEASUREMENT_ID}`}></Script>
       <Script id="app-script" strategy="lazyOnload">
         {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        gtag('config', '${process.env.FIREBASE_MEASUREMENT_ID}', {
           page_path: window.location.pathname,
           });
         `}
