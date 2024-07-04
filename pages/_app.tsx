be4e28b4b33e84223e49e36681f4cddbf2876888
Kevin <vnctkevin@gmail.com>
2024-07-05 01:07:48 +0700

    bugfix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { lazy } from 'react'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleTagManager } from '@next/third-parties/google'
 import { AppProps } from 'next/app'
-import { firebase } from '@/lib/firebase/firebaseClient'
 import Script from "next/script";
 
 import 'tailwindcss/tailwind.css'
@@ -21,6 +21,7 @@ export default function App({
   const { draftMode, token } = pageProps
   return (
     <>
+      <GoogleTagManager gtmId="G-YSBLVX27TT" />
       {draftMode ? (
         <PreviewProvider token={token}>
           <Component {...pageProps} />
