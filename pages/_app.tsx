ddb63aba34b6a984d4b50e69d9b237e53f79760e
Kevin <vnctkevin@gmail.com>
2024-07-05 15:08:23 +0700

    update analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ export default function App({
         <>
           <Component {...pageProps} />
           <Analytics />
-          <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.FIREBASE_MEASUREMENT_ID}`}></Script>
+          <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
           <Script id="app-script" strategy="lazyOnload">
             {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
